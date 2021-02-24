4c7cd1341337523841ee22094a531742196f0678
Simon Koelewijn <simon@seems.nl>
2021-02-24 14:24:00 +0100

    Tst2

--- _data/sites/eft.js
+++ _data/sites/eft.js
@@ -1,10 +1,10 @@
 module.exports = {
-	name: "SK", // optional, falls back to object key
+	name: "EFT", // optional, falls back to object key
 	description: "The default sites that get tested",
 	options: {
 		frequency: 60 * 23, // (in minutes), 23 hours
 	},
 	urls: [
-		"https://www.simonkoelewijn.nl"
+		"https://www.efteling.com/nl"
 	]
 };
