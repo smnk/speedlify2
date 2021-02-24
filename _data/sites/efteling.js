51afdd9c0af967b4fb026e4e27e8c1be3d0a72cf
Simon Koelewijn <simon@seems.nl>
2021-02-24 13:59:38 +0100

    Change sites

--- _data/sites/efteling.js
+++ _data/sites/efteling.js
@@ -1,5 +1,5 @@
 module.exports = {
-	name: "Sample", // optional, falls back to object key
+	name: "Efteling", // optional, falls back to object key
 	description: "The default sites that get tested",
 	options: {
 		frequency: 60 * 23, // (in minutes), 23 hours
